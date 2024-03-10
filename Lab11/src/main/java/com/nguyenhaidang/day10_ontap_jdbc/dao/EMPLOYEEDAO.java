3aa5fb70ae9cd3c8ffce2306b1aee436fc4c906e
NhDangcode <nguyenhaidangcode@gmail.com>
2024-03-10 21:21:50 +0700

    Lab11

---
+++ Lab11/src/main/java/com/nguyenhaidang/day10_ontap_jdbc/dao/EMPLOYEEDAO.java
@@ -0,0 +1,15 @@
+package com.nguyenhaidang.day10_ontap_jdbc.dao;
+
+import com.nguyenhaidang.day10_ontap_jdbc.entity.EMPLOYEEEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface EMPLOYEEDAO {
+    List<EMPLOYEEEntity> getAll();
+    List<EMPLOYEEEntity> getEMPLOYEEbyEMP_NAME(String EMP_NAME);
+    EMPLOYEEEntity getEMPLOYEEByid( BigDecimal id);
+    Boolean insertEMPLOYEE(EMPLOYEEEntity employee);
+    Boolean updateEMPLOYEE(EMPLOYEEEntity employee);
+    Boolean deleteEMPLOYEEByid(BigDecimal id );
+}
